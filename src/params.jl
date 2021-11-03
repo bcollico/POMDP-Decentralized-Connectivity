102d18467ff45a40220949150e05fb3e2828995f
Bradley <bcollico@stanford.edu>
2021-11-02 17:08:01 -0700

    updated params

--- src/params.jl
+++ src/params.jl
@@ -1,8 +1,12 @@
-struct paramsStruct
+struct ParamsStruct
     # Problem Setup
-    n_agents::Int
-    n_leaders::Int
-    grid_size::Int
+    num_agents::Int
+    num_leaders::Int
+
+    # Map Parameters
+    n_grid_size::Int
+    grid_dimension::Int
+    n_obstacles::Int
 
     # Rewards
     R_o::Int    # Negative reward for agent-obstacle collision
@@ -18,6 +22,7 @@ struct paramsStruct
     connect_thresh::Int # Maximum distance threshold for connectivity
 
     # Transition probability distribution (Discrete Gaussian)
+    σ_transition::Float64
     transition_bin_width::Float64
 
     # Collision Buffer Distance (L-nfty Norm)
@@ -29,6 +34,66 @@ struct paramsStruct
     α::Float64  # Learning Factor, in range(0, 1)
 end
 
-struct connectPOMDP <: POMDP{Array{CartesianIndices}, Array{Symbol}, Array{CartesianIndices}}
+function ParamsStruct()
 
+end
+
+struct ConnectPOMDP <: POMDP{Array{CartesianIndices}, Array{Symbol}, Array{CartesianIndices}}
+    # Problem Setup
+    num_agents::Int
+    num_leaders::Int
+    n_grid_size::Int
+
+    # Rewards
+    R_o::Int    # Negative reward for agent-obstacle collision
+    R_a::Int    # Negative reward for agent-agent    collision
+    R_λ::Int    # Negative reward for loss of connectivity
+
+    # Motion and Sensing Uncertainty
+    σ_obs::Float64    # Standard deviation of Gaussian measurement noise
+    σ_motion::Float64 # Standard deviation of Gaussian process     noise
+
+    # Connectivity Probability Distribution (Truncated Gaussian)
+    σ_connect::Float64  # Standard deviation
+    connect_thresh::Int # Maximum distance threshold for connectivity
+
+    # Transition probability distribution (Discrete Gaussian)
+    σ_transition::Float64
+    transition_bin_width::Float64
+
+    # Collision Buffer Distance (L-nfty Norm)
+    object_collision_buffer::Int 
+    agent_collision_buffer::Int
+
+    γ::Float64 # Discount Factor
+end
+
+function ConnectPOMDP(params::ParamsStruct)
+       # Problem Setup
+       num_agents  = params.num_agents
+       num_leaders = params.num_leaders
+       n_grid_size = params.n_grid_size
+   
+       # Rewards
+       R_o = params.R_o    # Negative reward for agent-obstacle collision
+       R_a = params.R_a    # Negative reward for agent-agent    collision
+       R_λ = params.R_λ    # Negative reward for loss of connectivity
+   
+       # Motion and Sensing Uncertainty
+       σ_obs    = params.σ_obs    # Standard deviation of Gaussian measurement noise
+       σ_motion = params.σ_motion # Standard deviation of Gaussian process     noise
+   
+       # Connectivity Probability Distribution (Truncated Gaussian)
+       σ_connect      = params.σ_connect  # Standard deviation
+       connect_thresh = params.connect_thresh # Maximum distance threshold for connectivity
+   
+       # Transition probability distribution (Discrete Gaussian)
+       σ_transition = params.σ_transition
+       transition_bin_width = params.transition_bin_width
+   
+       # Collision Buffer Distance (L-nfty Norm)
+       object_collision_buffer = params.object_collision_buffer
+       agent_collision_buffer = params.agent_collision_buffer
+   
+       γ = params.γ # Discount Factor
 end
