50d154e418d65610176accbe6ad83c5ca470bd25
Daniel Neamati <danineamati@gmail.com>
2021-11-02 17:59:21 -0700

    Start of plot states

--- src/map_generation.jl
+++ src/map_generation.jl
@@ -1,4 +1,6 @@
 
+include("params.jl")
+
 # The objective of this file is to set the obstacles in the POMDP problem
 
 """
@@ -23,15 +25,19 @@ Inputs:
 Outputs:
     obstacle coordinates as a CartesianIndex
 """
-function gen_rand_obstacle(n_grid::Int, grid_dimension::Int = 2)
+function gen_rand_obstacle(n_grid_size::Int, grid_dimension::Int = 2)
 
     obstacle_coords = zeros(Int, grid_dimension)
 
     for d in 1:grid_dimension
-        obstacle_coords[d] = rand(1:n_grid)
+        obstacle_coords[d] = rand(1:n_grid_size)
     end
 
     return CartesianIndex(obstacle_coords...)
+end
+
+function gen_rand_obstacle(p::ParamsStruct)
+    gen_rand_obstacle(p.n_grid_size, p.grid_dimension)
 end
 
 """
@@ -47,12 +53,16 @@ Outputs:
     Array of obstacle coordinates as an Array{CartesianIndex}
 
 """
-function gen_multiple_rand_obstacles(n_obstacles::Int, n_grid::Int,
+function gen_multiple_rand_obstacles(n_obstacles::Int, n_grid_size::Int,
                                      grid_dimension::Int = 2)
     all_obstacles = []
     for _ in 1:n_obstacles
-        push!(all_obstacles, gen_rand_obstacle(n_grid, grid_dimension))
+        push!(all_obstacles, gen_rand_obstacle(n_grid_size, grid_dimension))
     end
 
     return all_obstacles
+end
+
+function gen_multiple_rand_obstacles(p::ParamsStruct)
+    gen_multiple_rand_obstacles(p.n_obstacles, p.n_grid_size, p.grid_dimension)
 end
---
+++ src/plot_states.jl
@@ -0,0 +1,84 @@
+using Plots
+
+include("map_generation.jl")
+# include("states.jl")
+
+gr()
+
+"""
+    get_base_grid(p::ParamsStruct)
+
+Generate an empty 2D grid of the correct shape (n x n).
+
+Inputs:
+    n       Size of the grid `n`
+
+Outputs:
+    grid of zeros as an Array{Float64, 2}
+"""
+function get_base_grid(n_grid_size::Int)
+    # An zero'ed grid of size n x n
+    return zeros(n_grid_size, n_grid_size)
+end
+
+get_base_grid(p::ParamsStruct) = get_base_grid(p.n_grid_size)
+
+"""
+    add_obstacles_to_grid!(base_grid, obstacles_map::map)
+
+Include the obstacles in the grid.
+
+Inputs:
+    base_grid       Generated from `get_base_grid`
+    obstacles_map   The map with with obstacle locations
+
+Outputs:
+    None, in-place
+"""
+function add_obstacles_to_grid!(base_grid, obstacles_map::map)
+    # Now place the obstacles
+    for obstacle_location in obstacles_map.obstacleLocations
+        base_grid[obstacle_location] = -1
+    end
+end
+
+"""
+    add_bots_to_grid!(base_grip, s::Array{CartesianIndices})
+
+Include the robots in the grid.
+
+Inputs:
+    base_grid       Generated from `get_base_grid`
+    state           The states of the robots
+
+Outputs:
+    None, in-place
+"""
+function add_bots_to_grid!(base_grid, s::Array{CartesianIndices}, 
+                            num_agents::Int, num_leaders::Int)
+    # Now place the agents
+    for agent_ind in 1:num_agents
+        agent_location = s[agent_ind]
+        base_grid[agent_location] = 1
+    end
+
+    # Now place the leaders
+    for leader_ind in 1:num_leaders
+        leader_location = s[leader_ind + num_agents]
+        base_grid[leader_location] = 2
+    end
+end
+
+function add_bots_to_grid!(base_grid, s::Array{CartesianIndices}, p::ParamsStruct)
+    add_bots_to_grid!(base_grid, s, p.num_agents, p.num_leaders)
+end
+
+n = 10
+n_obs = 3
+
+ob_arr = gen_multiple_rand_obstacles(n_obs, n)
+ob_map = map(ob_arr)
+base_map = get_base_grid(10)
+add_obstacles_to_grid!(base_map, ob_map)
+
+hm = heatmap(1:n, 1:n, base_map, aspect_ratio=:equal)
