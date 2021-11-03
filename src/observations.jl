a510905dfc3951c3121e2ebe08d30188c82f5f2a
Bradley <bcollico@stanford.edu>
2021-11-03 14:48:04 -0700

    created discrete gaussian observation model. TODO: Check observation space

--- src/observations.jl
+++ src/observations.jl
@@ -1,35 +1,64 @@
 using POMDPs
 
 include("./params.jl")
+include("./states.jl")
+include("./transitions.jl")
 
 
-"""
-
-Since the observations are composed of the states + gaussian noise, the
-observation space much be continuous. 
-
-TODO: Represent the observation space continuously or as a dense discretized space
-"""
+"""Return the full observation space -- 𝒪 == 𝒮"""
 function POMDPs.observations(pomdp::ConnectPOMDP)
-    
+    return POMDPs.states(pomdp)
 end
 
 """
 
-Return the distribution over each state observation as a multivariate Gaussian
-distribution with mean μ = s and covariance Σ = diag([σ, ... , σ])
-
-The underlying assumption is that the measurement noise is zero-mean, Gaussian,
-and un-correlated between time instances.
 """
 function POMDPs.observation(pomdp::ConnectPOMDP, a::Array{Symbol}, s::Array{CartesianIndex{2},1})
-    σ = pomdp.σ_obs
+    # Total Number of Decision-Raking Robots
+    num_bots = pomdp.num_agents + pomdp.num_leaders
 
-    meas_cov = diagm(σ.*ones(2)) # Noise covariance for multivariate gaussian
-    o = []
-    for k = 1:length(s)
-        push!(o, MvNormal([s[k][1], s[k][2]], meas_cov))
+    a_ind = POMDPs.actionindex(pomdp, a)
+
+    𝒮 = POMDPs.states(pomdp)
+
+    𝒪 = []
+    for k = 1:num_bots
+        push!(𝒪, compute_observations(pomdp, num_bots, a_ind, s, 𝒮[1]))
     end
 
-    return o
+    return 𝒪
+end
+
+function compute_observations(
+    pomdp::ConnectPOMDP,
+    num_bots::Int,
+    a_ind::Array{Int},
+    s::Array{CartesianIndex{2},1},
+    𝒮::CartesianIndices{2,Tuple{Base.OneTo{Int64},Base.OneTo{Int64}}}
+)
+    𝒪 = []
+    for k = 1:num_bots
+        # find the order of likeliest states using the given action
+        sp_order = [pomdp.sp_order_table[:, 9]...]
+
+        # reset the bin distributions
+        p_bins = [pomdp.p_bins_observation...]
+        
+        p_bins[2:9] = (1-p_bins[1])/8 .* ones(8)
+
+        # sort the probability bins to the order of possible states
+        p_bins = p_bins[sortperm(sp_order)]
+
+        # compute reachable states and check out-of-bounds constraint
+        compute_p_reachable!(p_bins, s[k], 𝒮)
+
+        if abs(sum(p_bins) - 1) > 1e-4
+            @warn("Discrete Gaussian bins do not sum to 1 -- see transitions.jl")
+        end
+
+        # add categorical distribution to transition function array
+        push!(𝒪, Categorical(p_bins))
+    end
+    
+    return 𝒪
 end
